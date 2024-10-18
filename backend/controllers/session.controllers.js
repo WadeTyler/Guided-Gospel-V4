580c0bb5f6df889dffe4d83d8db40cd724f50f75
Tyler Wade <vaxonreal@gmail.com>
2024-10-18 18:56:52 -0400

    basic session route setup

---
+++ backend/controllers/session.controllers.js
@@ -0,0 +1,24 @@
+const db = require('../db/db');
+
+
+// Get all sessions for a user
+const getSessions = async (req, res) => {
+  try {
+    const userid = req.cookies.userid;
+    if (!userid) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const query = 'SELECT * FROM session WHERE userid = ?';
+    const [sessionsData] = await db.query(query, [userid]);
+    
+    return res.status(200).json(sessionsData);
+  } catch (error) {
+    console.log("Error in getSessions controller", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+module.exports = {
+
+}
--- backend/routes/session.routes.js
+++ backend/routes/session.routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const sessionController = require('../controllers/session.controllers');
 const router = express.Router();
 
-
+router.get('/', sessionController.getSessions);
 
 module.exports = router;
