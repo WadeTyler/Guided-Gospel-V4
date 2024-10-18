dd3e0d1f26492d7f49aa0125ce785a3e6b739127
Tyler Wade <vaxonreal@gmail.com>
2024-10-18 18:38:27 -0400

    updated getMe to a get method

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controllers');
 
-router.post("/", userController.getMe);
+router.get("/", userController.getMe);
 router.post("/signup", userController.signUp);
 router.post("/login", userController.login);
 router.post("/logout", userController.logout);
