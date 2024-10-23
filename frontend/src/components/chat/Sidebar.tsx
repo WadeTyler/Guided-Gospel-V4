3dbde95105542cb0da97552348a16178bfab5718
Tyler Wade <vaxonreal@gmail.com>
2024-10-23 12:21:00 -0400

    Removed 'Guided Gospel' from being a link

--- frontend/src/components/chat/Sidebar.tsx
+++ frontend/src/components/chat/Sidebar.tsx
@@ -125,7 +125,7 @@ const Sidebar = ({currentSessionid, setCurrentSessionid}: {currentSessionid: str
   return (
     <div className='bg-neutral-800 w-48 h-full flex flex-col shadow-[rgba(0,0,0)_0px_2px_8px] gap-8 px-3 py-3 absolute z-10 pb-12'>
 
-      <Link to="/" className="text-primary font-bold text-2xl mt-1 cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out">Guided Gospel</Link>
+      <h2 className="text-primary font-bold text-2xl mt-1 ">Guided Gospel</h2>
 
       <section className="text-white flex flex-row gap-2 items-center text-lg cursor-pointer hover:scale-105 transition-all duration-300 ease-in-out"
       onClick={() => setCurrentSessionid('')}
