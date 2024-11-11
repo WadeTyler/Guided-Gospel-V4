dacd1bf3239f5d3868b1e80333dd3711b90279d8
Tyler Wade <vaxonreal@gmail.com>
2024-11-11 15:08:07 -0500

    New Post functionality added to the sidebar

--- frontend/src/components/together/Sidebar.tsx
+++ frontend/src/components/together/Sidebar.tsx
@@ -3,24 +3,37 @@
   It contains the logo, the navigation links and the user profile.
 */
 
-import { Link } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query"; 
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"; 
 
 // Icons
-import { IconBellFilled, IconMessages, IconLogin, IconFriends, IconUsersGroup } from "@tabler/icons-react";
-
-
+import { IconBellFilled, IconMessages, IconLogin, IconFriends, IconUsersGroup, IconMessageFilled, IconX } from "@tabler/icons-react";
+import { SetStateAction, useState } from "react";
+import Loading from "../Loading";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
 
   const { data:authUser } = useQuery<User>({ queryKey: ['authUser'] });
 
+  // Creating new Post
+  const [posting, setPosting] = useState(false);
+
+
   return (
     <div className="fixed flex flex-col p-4 bg-neutral-800 dark:bg-darkaccent w-48 min-h-screen left-0 top-0 gap-8 z-50 shadow-[rgba(0,0,0)_0px_2px_8px]">
 
-      <h2 className="text-primary font-bold text-xl text-center mt-1 lg:mb-8 ">Guided Together</h2>
+      <h2 className="text-primary font-bold text-xl text-center mt-1">Guided Together</h2>
 
-      
+      {/* New Post */}
+      {authUser && 
+        <div className="flex gap-4 text-primary hover:text-white cursor-pointer" onClick={() => setPosting(true)}>
+          <IconMessageFilled />
+          <p>New Post</p>
+        </div>
+      }
+
+      {posting && <Posting setPosting={setPosting} />}
 
 
       {/* Nav Section */}
@@ -63,6 +76,74 @@ const Sidebar = () => {
             <p>Login</p>
         </Link>
       }
+    </div>
+  )
+}
+
+// Posting Component - New Post
+const Posting = ({setPosting}: {setPosting: React.Dispatch<SetStateAction<boolean>>;}) => {
+
+  const queryClient = useQueryClient();
+
+  const { data:authUser } = useQuery<User>({ queryKey: ['authUser'] });
+  const navigate = useNavigate();
+
+  const [content, setContent] = useState<string>('');
+  
+  // create Post
+  const { mutate:createPost, isPending:sendingPost } = useMutation({
+
+    mutationFn: async ({ text: content }:{ text:string; }) => {
+      try {
+        const response = await fetch('/api/together/posts', {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ content })
+        });
+        const data = await response.json();
+        
+        if (!response.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+
+        return data;
+      } catch (error) {
+        throw new Error((error as Error).message || "Something went wrong");
+      }
+    },
+    onSuccess: () => {
+      navigate(`/together/users/${authUser?.username}`);
+      queryClient.invalidateQueries({ queryKey: ['targetPosts'] });
+      toast.success("Post Created Successfully");
+      setPosting(false);
+    },
+    onError: (error: Error) => {
+      toast.error(error.message || "Something went wrong");
+    }
+  })
+
+  // submit create Post
+  const handleSubmitPost = () => {
+    createPost({text: content});
+  }
+
+  return (
+    <div className="fixed w-full top-0 left-0 min-h-screen bg-[rgba(0,0,0,.8)] flex items-center justify-center z-50">
+      <div className="w-[40rem] h-96 bg-white rounded-2xl flex flex-col items-end justify-center dark:text-darktext dark:bg-darkbg relative p-4 gap-4">
+        <IconX className="absolute top-4 right-4 hover:text-red-500 cursor-pointer" onClick={() => setPosting(false)} />
+        <h4 className="text-primary text-3xl text-end mt-8 w-full">New Post</h4>
+        <textarea name="createpost" id="createpost" 
+          className="w-full h-full bg-white dark:bg-darkbg form-input-bar resize-none" placeholder="Send a new Post!"
+          value={content}
+          onChange={(e) => {
+            e.preventDefault();
+            setContent(e.target.value);
+          }} />
+          {!sendingPost && <button className="flex gap-2 items-center hover:text-primary transition-all duration-300 ease-in-out " onClick={handleSubmitPost}><IconMessageFilled /> Send Post</button>}
+          {sendingPost && <Loading size="md" cn="text-primary" />}
+      </div>
     </div>
   )
 }
