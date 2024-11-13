9e6279fcfebb9f2b652f70b0f94be2bc8a0e359e
Tyler Wade <vaxonreal@gmail.com>
2024-11-13 16:28:35 -0500

    changed to normal 'new Date()' format

--- backend/lib/utils/sqlFormatting.js
+++ backend/lib/utils/sqlFormatting.js
@@ -1,6 +1,6 @@
 
 const getTimestampInSQLFormat = () => {
-  const timestamp = new Date().toISOString().replace('T', ' ').substring(0, 19);
+  const timestamp = new Date();
   return timestamp;
 }
 
