ade0f69633122ccb77f6c4cbc6a67c6e5f2f0961
Tyler Wade <vaxonreal@gmail.com>
2024-11-13 10:09:24 -0500

    Daily Rates message limit changed from 50 to 12

--- frontend/src/pages/Rates.tsx
+++ frontend/src/pages/Rates.tsx
@@ -9,12 +9,12 @@ const Rates = () => {
         <h2 className="text-3xl text-primary">How do Daily Message Rates Work?</h2>
         <section>
           <p className="text-xl">Daily Limit</p>
-          <p>By default Guided Gospel provides a limit of 50 messages per day. Once, you send 50 messages, you will no longer be able to send any more. Because Guided Gospel is a free service, a daily limit is in place to prevent malicious users from abusing our service.</p>
+          <p>By default Guided Gospel provides a limit of 12 messages per day. Once, you send 12 messages, you will no longer be able to send any more. Because Guided Gospel is a free service, a daily limit is in place to prevent malicious users from abusing our service.</p>
         </section>
 
         <section>
           <p className="text-xl">How do I get more?</p>
-          <p>Daily Limits reset at 12:00 AM EST (Midnight) every night. At the moment, there currently is no official way to get more than 50 message rates per day, but there is plans for in the future.</p>
+          <p>Daily Limits reset at 12:00 AM EST (Midnight) every night. At the moment, there currently is no official way to get more than 12 message rates per day, but there is plans for in the future.</p>
         </section>
 
         <Link to="/settings" className="text-primary underline">Go Back</Link>
